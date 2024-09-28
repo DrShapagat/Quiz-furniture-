c78820686164efaeb022e9b2d481969165110876
DrShapagat <shapagat765@gmail.com>
2024-09-29 00:50:47 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function App() {
-  return <h1>Welcome to the Quiz App!</h1>;
+  return (
+    <div>
+      <h1>Добро пожаловать в приложение Quiz!</h1>
+      <p>Здесь появятся вопросы и ответы.</p>
+    </div>
+  );
 }
 
 export default App;
